1137f69122e6533f5c2e8e3b3522a679b6f0d86f
Phil Barker <phil.barker@pjjk.co.uk>
2019-05-03 16:45:18 +0100

    Updating testing to pytest

--- tests.py
+++ tests.py
@@ -1,4 +1,4 @@
-import markdown, unittest
+import markdown, pytest
 from ocxmd import OCXMetadata
 TESTINPUT = '''---
 "@id": "#lesson1"
@@ -36,15 +36,10 @@ HTMLEXPECTED = '''<script type="application/ld+json">{"@context": ["http://schem
 <script type="application/ld+json">{"@context": ["http://schema.org", {"oer": "http://oerschema.org/"}, {"ocx": "https://github.com/K12OCX/k12ocx-specs/"}], "@id": "#activity1", "@type": ["oer:Activity", "CreativeWork"], "name": "Test Activity 1.1", "learningResourceType": "Activity"}</script>'''
 
 METADATAEXPECTED = {1: {'@context': ['http://schema.org', {'oer': 'http://oerschema.org/'}, {'ocx': 'https://github.com/K12OCX/k12ocx-specs/'}], '@id': '#lesson1', 'name': 'Test Lesson 1', '@type': ['oer:Lesson', 'CreativeWork'], 'learningResourceType': 'LessonPlan', 'hasPart': {'@id': '#activity1'}, 'author': {'@type': 'Person', 'name': 'Fred Blogs'}}, 2: {'@context': ['http://schema.org', {'oer': 'http://oerschema.org/'}, {'ocx': 'https://github.com/K12OCX/k12ocx-specs/'}], '@id': '#activity1', '@type': ['oer:Activity', 'CreativeWork'], 'name': 'Test Activity 1.1', 'learningResourceType': 'Activity'}}
-class TestOCXMD(unittest.TestCase):
-    md = markdown.Markdown(extensions = ['ocxmd'])
-    html = md.convert(TESTINPUT)
-    print(md.meta)
-    print(html)
-    def test_html(self):
-        self.assertEqual(self.html, HTMLEXPECTED)
-    def test_md(self):
-        self.assertEqual(self.md.meta, METADATAEXPECTED)
 
-if '__main__' == __name__:
-    unittest.main()
+
+md = markdown.Markdown(extensions = ['ocxmd'])
+html = md.convert(TESTINPUT)
+def test1():
+    assert md.meta == METADATAEXPECTED
+    assert html == HTMLEXPECTED
