e5d5de747804acf438583f5ce5fc9255d531b65f
Phil Barker <phil.barker@pjjk.co.uk>
2019-05-06 18:28:58 +0100

    bumping version (changes to specifying context)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="ocxmd",
-    version="0.1",
+    version="0.2",
     py_modules=["ocxmd"],
     install_requires=["markdown>=3.0", "PyYAML>=3.13"],
     author="Phil Barker",
