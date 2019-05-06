c6374a24e9616bed647020bb9cfebfe1794523b9
Phil Barker <phil.barker@pjjk.co.uk>
2019-05-06 18:30:50 +0100

    blacking

--- test.py
+++ test.py
@@ -1,29 +1,32 @@
 import markdown, pytest
 from ocxmd import OCXMetadata
-TESTINPUT_1_1 = '''---
+
+TESTINPUT_1_1 = """---
 "@context": "http://schema.org/"
 "@id": "#lesson1"
 "@type": "CreativeWork"
 ---
 #YAML to JSON-LD test
 I started with some YAML and turned it into JSON-LD
-'''
-TESTINPUT_1_2 = '''---
+"""
+TESTINPUT_1_2 = """---
 "@id": "#lesson1"
 "@type": "CreativeWork"
 ---
 #YAML to JSON-LD test
 I started with some YAML and turned it into JSON-LD
-'''
+"""
 
-HTMLEXPECTED_1 = '''<script type="application/ld+json">{"@context": "http://schema.org/", "@id": "#lesson1", "@type": "CreativeWork"}</script>
+HTMLEXPECTED_1 = """<script type="application/ld+json">{"@context": "http://schema.org/", "@id": "#lesson1", "@type": "CreativeWork"}</script>
 
 <h1>YAML to JSON-LD test</h1>
-<p>I started with some YAML and turned it into JSON-LD</p>'''
+<p>I started with some YAML and turned it into JSON-LD</p>"""
 
-METADATAEXPECTED_1 = {1: {"@context": "http://schema.org/", "@id": "#lesson1", "@type": "CreativeWork"}}
+METADATAEXPECTED_1 = {
+    1: {"@context": "http://schema.org/", "@id": "#lesson1", "@type": "CreativeWork"}
+}
 
-TESTINPUT = '''---
+TESTINPUT = """---
 "@id": "#lesson1"
 name: "Test Lesson 1"
 "@type":
@@ -50,53 +53,108 @@ Here is some more YAML
 name: "Test Activity 1.1"
 learningResourceType: Activity
 ---
-'''
-HTMLEXPECTED_3 = '''<script type="application/ld+json">{"@context": ["http://schema.org", {"oer": "http://oerschema.org/"}, {"ocx": "https://github.com/K12OCX/k12ocx-specs/"}], "@id": "#lesson1", "name": "Test Lesson 1", "@type": ["oer:Lesson", "CreativeWork"], "learningResourceType": "LessonPlan", "hasPart": {"@id": "#activity1"}, "author": {"@type": "Person", "name": "Fred Blogs"}}</script>
+"""
+HTMLEXPECTED_3 = """<script type="application/ld+json">{"@context": ["http://schema.org", {"oer": "http://oerschema.org/"}, {"ocx": "https://github.com/K12OCX/k12ocx-specs/"}], "@id": "#lesson1", "name": "Test Lesson 1", "@type": ["oer:Lesson", "CreativeWork"], "learningResourceType": "LessonPlan", "hasPart": {"@id": "#activity1"}, "author": {"@type": "Person", "name": "Fred Blogs"}}</script>
 
 <h1>YAML to JSON-LD test</h1>
 <p>I started with some YAML and turned it into JSON-LD</p>
 <p>Here is some more YAML</p>
-<script type="application/ld+json">{"@context": ["http://schema.org", {"oer": "http://oerschema.org/"}, {"ocx": "https://github.com/K12OCX/k12ocx-specs/"}], "@id": "#activity1", "@type": ["oer:Activity", "CreativeWork"], "name": "Test Activity 1.1", "learningResourceType": "Activity"}</script>'''
+<script type="application/ld+json">{"@context": ["http://schema.org", {"oer": "http://oerschema.org/"}, {"ocx": "https://github.com/K12OCX/k12ocx-specs/"}], "@id": "#activity1", "@type": ["oer:Activity", "CreativeWork"], "name": "Test Activity 1.1", "learningResourceType": "Activity"}</script>"""
 
-HTMLEXPECTED_2 = '''<script type="application/ld+json">{"@context": "http://schema.org", "@id": "#lesson1", "name": "Test Lesson 1", "@type": ["oer:Lesson", "CreativeWork"], "learningResourceType": "LessonPlan", "hasPart": {"@id": "#activity1"}, "author": {"@type": "Person", "name": "Fred Blogs"}}</script>
+HTMLEXPECTED_2 = """<script type="application/ld+json">{"@context": "http://schema.org", "@id": "#lesson1", "name": "Test Lesson 1", "@type": ["oer:Lesson", "CreativeWork"], "learningResourceType": "LessonPlan", "hasPart": {"@id": "#activity1"}, "author": {"@type": "Person", "name": "Fred Blogs"}}</script>
 
 <h1>YAML to JSON-LD test</h1>
 <p>I started with some YAML and turned it into JSON-LD</p>
 <p>Here is some more YAML</p>
-<script type="application/ld+json">{"@context": "http://schema.org", "@id": "#activity1", "@type": ["oer:Activity", "CreativeWork"], "name": "Test Activity 1.1", "learningResourceType": "Activity"}</script>'''
+<script type="application/ld+json">{"@context": "http://schema.org", "@id": "#activity1", "@type": ["oer:Activity", "CreativeWork"], "name": "Test Activity 1.1", "learningResourceType": "Activity"}</script>"""
 
 
-METADATAEXPECTED_2 = {1: {'@context': 'http://schema.org', '@id': '#lesson1', 'name': 'Test Lesson 1', '@type': ['oer:Lesson', 'CreativeWork'], 'learningResourceType': 'LessonPlan', 'hasPart': {'@id': '#activity1'}, 'author': {'@type': 'Person', 'name': 'Fred Blogs'}}, 2: {'@context': 'http://schema.org', '@id': '#activity1', '@type': ['oer:Activity', 'CreativeWork'], 'name': 'Test Activity 1.1', 'learningResourceType': 'Activity'}}
+METADATAEXPECTED_2 = {
+    1: {
+        "@context": "http://schema.org",
+        "@id": "#lesson1",
+        "name": "Test Lesson 1",
+        "@type": ["oer:Lesson", "CreativeWork"],
+        "learningResourceType": "LessonPlan",
+        "hasPart": {"@id": "#activity1"},
+        "author": {"@type": "Person", "name": "Fred Blogs"},
+    },
+    2: {
+        "@context": "http://schema.org",
+        "@id": "#activity1",
+        "@type": ["oer:Activity", "CreativeWork"],
+        "name": "Test Activity 1.1",
+        "learningResourceType": "Activity",
+    },
+}
 
-METADATAEXPECTED_3 = {1: {'@context': ['http://schema.org', {'oer': 'http://oerschema.org/'}, {'ocx': 'https://github.com/K12OCX/k12ocx-specs/'}], '@id': '#lesson1', 'name': 'Test Lesson 1', '@type': ['oer:Lesson', 'CreativeWork'], 'learningResourceType': 'LessonPlan', 'hasPart': {'@id': '#activity1'}, 'author': {'@type': 'Person', 'name': 'Fred Blogs'}}, 2: {'@context': ['http://schema.org', {'oer': 'http://oerschema.org/'}, {'ocx': 'https://github.com/K12OCX/k12ocx-specs/'}], '@id': '#activity1', '@type': ['oer:Activity', 'CreativeWork'], 'name': 'Test Activity 1.1', 'learningResourceType': 'Activity'}}
+METADATAEXPECTED_3 = {
+    1: {
+        "@context": [
+            "http://schema.org",
+            {"oer": "http://oerschema.org/"},
+            {"ocx": "https://github.com/K12OCX/k12ocx-specs/"},
+        ],
+        "@id": "#lesson1",
+        "name": "Test Lesson 1",
+        "@type": ["oer:Lesson", "CreativeWork"],
+        "learningResourceType": "LessonPlan",
+        "hasPart": {"@id": "#activity1"},
+        "author": {"@type": "Person", "name": "Fred Blogs"},
+    },
+    2: {
+        "@context": [
+            "http://schema.org",
+            {"oer": "http://oerschema.org/"},
+            {"ocx": "https://github.com/K12OCX/k12ocx-specs/"},
+        ],
+        "@id": "#activity1",
+        "@type": ["oer:Activity", "CreativeWork"],
+        "name": "Test Activity 1.1",
+        "learningResourceType": "Activity",
+    },
+}
 
 
-YAML_CONTEXT = '''
+YAML_CONTEXT = """
 "@context":
     - "http://schema.org"
     - "oer": "http://oerschema.org/"
     - "ocx": "https://github.com/K12OCX/k12ocx-specs/"
-'''
+"""
+
 
 def test1_1():
-        md = markdown.Markdown(extensions = ['ocxmd'])
-        html = md.convert(TESTINPUT_1_1)
-        assert md.meta == METADATAEXPECTED_1
-        assert html == HTMLEXPECTED_1
+    md = markdown.Markdown(extensions=["ocxmd"])
+    html = md.convert(TESTINPUT_1_1)
+    assert md.meta == METADATAEXPECTED_1
+    assert html == HTMLEXPECTED_1
+
+
 def test1_2():
-        md = markdown.Markdown(extensions = ['ocxmd'], extension_configs = {'ocxmd': {'context' : '\'@context\' : \'http://schema.org\''}})
-        html = md.convert(TESTINPUT_1_1)
-        assert md.meta == METADATAEXPECTED_1
-        assert html == HTMLEXPECTED_1
+    md = markdown.Markdown(
+        extensions=["ocxmd"],
+        extension_configs={"ocxmd": {"context": "'@context' : 'http://schema.org'"}},
+    )
+    html = md.convert(TESTINPUT_1_1)
+    assert md.meta == METADATAEXPECTED_1
+    assert html == HTMLEXPECTED_1
+
 
 def test2():
-        md = markdown.Markdown(extensions = ['ocxmd'], extension_configs = {'ocxmd': {'context' : '\'@context\' : \'http://schema.org\''}})
-        html = md.convert(TESTINPUT)
-        assert md.meta == METADATAEXPECTED_2
-        assert html == HTMLEXPECTED_2
+    md = markdown.Markdown(
+        extensions=["ocxmd"],
+        extension_configs={"ocxmd": {"context": "'@context' : 'http://schema.org'"}},
+    )
+    html = md.convert(TESTINPUT)
+    assert md.meta == METADATAEXPECTED_2
+    assert html == HTMLEXPECTED_2
+
 
 def test3():
-    md = markdown.Markdown(extensions = ['ocxmd'], extension_configs = {'ocxmd': {'context' : YAML_CONTEXT}} )
+    md = markdown.Markdown(
+        extensions=["ocxmd"], extension_configs={"ocxmd": {"context": YAML_CONTEXT}}
+    )
     html = md.convert(TESTINPUT)
     assert md.meta == METADATAEXPECTED_3
     assert html == HTMLEXPECTED_3
