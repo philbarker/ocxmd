f7ac455626f383974cdd82e4e49f718c4da21a90
Phil Barker <phil.barker@pjjk.co.uk>
2019-05-23 16:13:22 +0100

    processing turtle

--- ocxmd.py
+++ ocxmd.py
@@ -1,11 +1,8 @@
 from markdown.extensions import Extension
 from markdown.preprocessors import Preprocessor
-import yaml, json
-
-OCX_YAML_STARTER = "---"
+import yaml, json, rdflib
 
 SCRIPT_STARTER = '<script type="application/ld+json">'
-
 
 class OCXMetadata(Extension):
     """Python-Markdown extension for parsing OCX metadata from YAML."""
@@ -20,7 +17,6 @@ class OCXMetadata(Extension):
         self.md = md
         self.md.context = self.getConfig("context")
         md.preprocessors.register(OCXMetadataPreprocessor(md), "ocxmetadata", 28)
-
 
 class OCXMetadataPreprocessor(Preprocessor):
     def process_yaml_block(self, lines, index):
@@ -38,6 +34,25 @@ class OCXMetadataPreprocessor(Preprocessor):
             else: # if it's not the end of the YAML, add line to YAML
                 yaml_block.append(line)
 
+    def process_ttl_block(self, lines, index) :
+        ttl_block = [] # start with empty list context
+        g = rdflib.Graph()
+        while lines:  # loop processing TTL block
+            line = lines.pop(0)
+            if line == "---":  # should be the end of a YAML block
+                self.ttl[index] = "\n".join(ttl_block)
+                print(self.ttl[index])
+                g.parse(data=self.ttl[index], format="turtle")
+                json_md = g.serialize(format='json-ld', indent=4).decode("utf-8")
+                new_line = (
+                    SCRIPT_STARTER
+                    + json_md
+                    + "</script>"
+                )
+                return new_line # leave loop for processing YAML block
+            else: # if it's not the end of the YAML, add line to YAML
+                ttl_block.append(line)
+
     def run(self, lines):
         new_lines = []
         self.yaml = {}
@@ -49,10 +64,13 @@ class OCXMetadataPreprocessor(Preprocessor):
             if line == "---":  # should be start of a YAML block
                 yaml_count += 1
                 new_lines.append( self.process_yaml_block(lines, yaml_count) )
+            elif line == "---TTL":  # should be start of a TTL block
+                ttl_count += 1
+                new_lines.append( self.process_ttl_block(lines, ttl_count) )
             else:
                 new_lines.append(line)
-        if self.yaml:
-            self.md.meta = self.yaml
+        if self.yaml or self.ttl:
+            self.md.meta = {**self.yaml, **self.ttl}
         else:
             self.md.meta = None
         return new_lines
